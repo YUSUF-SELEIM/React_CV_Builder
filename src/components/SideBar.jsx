b8751ec1ce4d1befba09dab7d7ba10e5ed3f22aa
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-01-16 00:27:47 +0200

    Added SideBar Component

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,20 +1,40 @@
-import { Sidebar } from 'flowbite-react';
-import { HiDocumentText, HiCog } from 'react-icons/hi';
+/* eslint-disable react/prop-types */
+import { Sidebar } from "flowbite-react";
+import { HiDocumentText, HiCog } from "react-icons/hi";
 
-'use client';
+("use client");
 
-export default function SideBar(){
+export default function SideBar({ handleTabClick }) {
   return (
-    <Sidebar className='m-3 shadow-lg'>
-      <Sidebar.Logo href="#" img="./src/assets/resume.png" imgAlt="Flowbite logo">
-       CV Builder
+    <Sidebar className="m-3 w-[15%] shadow-lg">
+      <Sidebar.Logo
+        href="#"
+        img="./src/assets/resume.png"
+        imgAlt="Flowbite logo"
+        className="justify-center"
+      >
       </Sidebar.Logo>
       <Sidebar.Items>
         <Sidebar.ItemGroup>
-          <Sidebar.Item href="#" icon={HiDocumentText}>
+          <Sidebar.Item
+            href="#"
+            icon={HiDocumentText}
+            onClick={() => {
+              handleTabClick("0");
+              console.log("Content is Selected 0");
+            }}
+          >
             Content
           </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiCog}>
+          <Sidebar.Item
+            href="#"
+            icon={HiCog}
+            onClick={() => {
+              console.log("Customize is Selected 1");
+
+              handleTabClick("1");
+            }}
+          >
             Customize
           </Sidebar.Item>
         </Sidebar.ItemGroup>
@@ -22,4 +42,3 @@ export default function SideBar(){
     </Sidebar>
   );
 }
-
