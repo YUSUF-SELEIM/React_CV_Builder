b159620436ddcffd43bc74edd64ae31f2f4eb6b8
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-01-16 04:34:20 +0200

    added a button that loads my data in cv

---
+++ src/components/loadExample.jsx
@@ -0,0 +1,6 @@
+/* eslint-disable react/prop-types */
+import { Button } from "flowbite-react";
+
+export default function LoadExample({ handleDebugData }) {
+  return <Button onClick={handleDebugData}>Load Example</Button>;
+}
